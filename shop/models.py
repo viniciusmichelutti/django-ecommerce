8f815e0d7afdea7aba7cce5a39ec4f5d9efc6155
Vinícius Michelutti <vinicius@meuspedidos.com.br>
2017-05-15 18:47:19 -0300

    Resolving url reverse

--- shop/models.py
+++ shop/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
+        return reverse('shop:product_list_by_category', args=(self.slug,))
 
 
 class Product(models.Model):
@@ -38,4 +38,4 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.slug])
+        return reverse('shop:product_detail', args=(self.slug,))
--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,6 @@ from shop import views
 
 urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
-    url(r'^category/(?P<slug>[-\w]+/)/$', views.product_list, name='product_list_by_category'),
-    url(r'^product/(?P<slug>[-\w]+/)/$', views.product_detail, name='product_detail'),
+    url(r'^category/(?P<category_slug>[\w\-]+)/$', views.product_list, name='product_list_by_category'),
+    url(r'^product/(?P<product_slug>[\w\-]+)/$', views.product_detail, name='product_detail'),
 ]
